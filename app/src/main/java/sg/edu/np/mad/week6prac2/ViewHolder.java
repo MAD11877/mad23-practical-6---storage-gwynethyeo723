40c1fa0905b754a9897dbe6ec11deafc4949ee68
gwynethyeo723 <s10222843@connect.np.edu.sg>
2023-06-17 00:54:28 +0800

    finished

---
+++ app/src/main/java/sg/edu/np/mad/week6prac2/ViewHolder.java
@@ -0,0 +1,22 @@
+package sg.edu.np.mad.week6prac2;
+
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    ImageView image;
+    TextView txt;
+    TextView txtdes;
+    public ViewHolder(View itemView){
+        super(itemView);
+        image = itemView.findViewById(R.id.imageView3);
+        txt = itemView.findViewById(R.id.textView3);
+        txtdes = itemView.findViewById(R.id.textView4);
+    }
+
+}
+
